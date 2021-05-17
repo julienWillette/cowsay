a4c3580ffc811d29ef194ab4a739076389f4adeb
julien willette <julien.willette@gmail.com>
2021-05-17 12:39:16 +0200

    fixe bug

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const cowsay = require("cowsay");
+const myInfos = require("./information.js");
 const myInformation = require('./information.js');
 
 
 console.log(cowsay.say({
-	text : "Hi, I'm " + myInfo.name + " from the famous " + myInfo.campus + " campus!",
+	text : "Hi, I'm " + myInfos.name + " from the famous " + myInfos.campus + " campus!",
 	e : "oO",
 	T : "U "
 }));
